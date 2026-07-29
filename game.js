c83c2d1d4ba0bd3d49b335ee38cb27cdb1bd99bf
chabdilov28-art <chabdilov28@gmail.com>
2026-07-29 23:46:45 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,20 @@
 const startBtn = document.getElementById("startBtn");
 
 startBtn.addEventListener("click", function () {
-    alert("ACHX Moto Challenge boshlandi!");
+    document.querySelector(".menu").style.display = "none";
+
+    document.body.innerHTML += `
+    <div id="game" style="
+        width:100%;
+        height:100vh;
+        background:#444;
+        color:white;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        font-size:32px;
+        font-family:Arial;">
+        🏍️ ACHX Moto Challenge <br><br>
+        O'yin qurilmoqda...
+    </div>`;
 });
