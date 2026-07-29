b8e410f065a3e389dfe2ba374abdf8a5ee6ad974
chabdilov28-art <chabdilov28@gmail.com>
2026-07-30 00:08:55 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
-alert("game.js ishladi"); 
+const startBtn = document.getElementById("startBtn");
+
+startBtn.addEventListener("click", function () {
+    document.getElementById("menu").style.display = "none";
+
+    document.body.innerHTML = `
+    <div style="
+        width:100%;
+        height:100vh;
+        background:#444;
+        color:white;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        font-size:32px;
+        font-family:Arial;
+    ">
+        🏍️ ACHX Moto Challenge<br><br>
+        O'yin qurilmoqda...
+    </div>`;
+});
